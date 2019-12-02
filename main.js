5b18685acab13a3a2c6af78ff6a8362e73c8d010
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-12-02 14:36:56 +0100

    Creazione Files

---
+++ main.js
@@ -0,0 +1,5 @@
+// Dobbiamo replicare la famosa app di messaggistica in versione web e come dicevamo a lezione, procederemo per milestone.
+// Nome repo: js-html-css-boolzap
+// Oggi pomeriggio iniziate ad occuparvi della milestone 1, che prevede di:
+// creare la grafica di boolzapp (whatsapp web)
+// dare la possibilità all'utente di inviare un messaggio: scrivendo nel campo di testo in basso e premendo un pulsante, il messaggio comparirà nel contenitore della conversazione. Per fare questo punto, vi consiglio di usare la funzione clone() che abbiamo visto insieme per utilizzare un div template a mo' di "stampo".
