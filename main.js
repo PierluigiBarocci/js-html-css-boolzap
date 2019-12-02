7b0d995f7b21d128e2b337366bc459472e710f38
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-12-02 14:43:19 +0100

    Aggiunto l'obiettivo di giornata al main.js, ci aspetta un bel lavoro

--- main.js
+++ main.js
@@ -3,3 +3,9 @@
 // Oggi pomeriggio iniziate ad occuparvi della milestone 1, che prevede di:
 // creare la grafica di boolzapp (whatsapp web)
 // dare la possibilità all'utente di inviare un messaggio: scrivendo nel campo di testo in basso e premendo un pulsante, il messaggio comparirà nel contenitore della conversazione. Per fare questo punto, vi consiglio di usare la funzione clone() che abbiamo visto insieme per utilizzare un div template a mo' di "stampo".
+
+// Milestone 1
+// ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+// dall’interlocutore (bianco) assegnando due classi CSS diverse
+// ● Aggiunta di un messaggio : l’utente scrive un testo nella parte bassa e cliccando
+// invia il testo viene aggiunto al thread sopra, come messaggio verde
